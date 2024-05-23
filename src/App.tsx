83721834e9c114eca33ed34065bd0657230c316b
Lukáš Jarmara <jarmaralukas123@gmail.com>
2024-05-23 15:42:28 +0200

    Imported google font's icons, added basic navigation using them

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Nav from "./Components/Nav.tsx";
+const App: React.FC = () => {
+  return (
+    <>
+      <Nav />
+    </>
+  );
+};
+
+export default App;
---
+++ src/Components/Nav.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Nav: React.FC = () => {
+  return (
+    <nav>
+      <div>
+        <span className="material-symbols-outlined">home</span>
+        <span className="material-symbols-outlined">code</span>
+        <span className="material-symbols-outlined">person</span>
+        <span className="material-symbols-outlined">mail</span>
+      </div>
+      <div></div>
+    </nav>
+  );
+};
+
+export default Nav;
