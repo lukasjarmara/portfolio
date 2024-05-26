be616343b5ebd886f5b0275b3cd02b2252db2f0d
Lukáš Jarmara <jarmaralukas123@gmail.com>
2024-05-26 11:59:25 +0200

    Added skills to the about section

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -2,9 +2,42 @@ import React from "react";
 
 const About: React.FC = () => {
   return (
-    <div className="bg-light h-screen pt-28">
-      <p>My Skills</p>
+    <div className="bg-light h-screen pt-28 flex flex-col items-center">
+      <p className="text-2xl font-bold mb-4">My Skills</p>
+      <div className="w-3/4 mb-4 flex flex-row gap-4 justify-center">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
+          alt="React"
+          className="h-24 w-24 mb-4"
+        />
+        <img
+          src="https://cdn.worldvectorlogo.com/logos/typescript.svg"
+          alt="TypeScript"
+          className="h-24 w-24 mb-4"
+        />
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png"
+          alt="JavaScript"
+          className="h-24 w-24 mb-4"
+        />
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/d/d5/Tailwind_CSS_Logo.svg"
+          alt="Tailwind CSS"
+          className="h-24 w-24 mb-4"
+        />
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg"
+          alt="HTML"
+          className="h-24 w-24 mb-4"
+        />
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg"
+          alt="CSS"
+          className="h-24 w-24 mb-4"
+        />
+      </div>
     </div>
   );
 };
+
 export default About;
--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -2,9 +2,11 @@ import React from "react";
 
 const Hero: React.FC = () => {
   return (
-    <div className="bg-light h-screen pt-28">
-      <p>Hi, I'm Lukas</p>
+    <div className="bg-light h-screen flex flex-col justify-center items-center">
+      <p className="text-3xl font-bold">Hi, I'm Lukas</p>
+      <p className="text-2xl">A Front-End Developer</p>
     </div>
   );
 };
+
 export default Hero;
