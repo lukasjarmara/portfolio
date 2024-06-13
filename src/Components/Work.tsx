b56542c3d8f083c90cf42f4801bc347b5dc1b06b
Lukáš Jarmara <jarmaralukas123@gmail.com>
2024-06-13 17:27:27 +0200

    Added more work to the Work component

--- src/Components/Work.tsx
+++ src/Components/Work.tsx
@@ -2,26 +2,22 @@ import React from "react";
 
 const projects = [
   {
-    imgSrc: "placeholder-image-1.jpg",
-    title: "Project 1",
-    description: "Description of Project 1",
-    link: "https://example.com/project1",
+    title: "Future Port Youth",
+    description: "A full-fletch website for a company I am working with",
+    link: "https://futureportyouth.com/",
   },
   {
-    imgSrc: "/Assets/Chatroom.jpg",
     title: "Chatroom",
     description:
       "A chatroom application built with React and Firebase. Includes real-time messaging and room selection",
     link: "https://jarmara-chatroom.web.app/",
   },
   {
-    imgSrc: "placeholder-image-3.jpg",
     title: "Project 3",
     description: "Description of Project 3",
     link: "https://example.com/project3",
   },
   {
-    imgSrc: "placeholder-image-4.jpg",
     title: "Project 4",
     description: "Description of Project 4",
     link: "https://example.com/project4",
@@ -45,11 +41,6 @@ const Work: React.FC = () => {
             className="block transform scale-[0.99] transition-transform duration-300 hover:scale-[1.02]"
           >
             <div className="bg-secondaryLight p-4 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
-              <img
-                src={project.imgSrc}
-                alt={project.title}
-                className="w-full h-64 object-contain rounded-t-lg transition-transform duration-300"
-              />
               <div className="p-4">
                 <h3 className="text-xl font-semibold mb-2">{project.title}</h3>
                 <p className="text-dark text-base">{project.description}</p>
